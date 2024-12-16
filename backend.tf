1ae2155267e4b71c99696346d54f19135c9d51ca
kalusingh rajput <87700418+itksrajput@users.noreply.github.com>
2024-12-16 20:32:18 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,10 +1,10 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-state-eks-20241216-xyz1234"  # Ensure this is globally unique
-    region         = "us-west-1"
-    key            = "eks/terraform.tfstate"
-    dynamodb_table = "terraform-lock-table"
-    encrypt        = true
+    bucket         = "terraform-state-eks-20241216-xyz126"  # Ensure this is globally unique
+    region         = "us-east-1"                             # Specify your region
+    key            = "eks/terraform.tfstate"                 # Path within the S3 bucket for the state file
+    dynamodb_table = "terraform-lock-table"                  # DynamoDB table for state locking
+    encrypt        = true                                    # Enable server-side encryption of state file
   }
 }
 
